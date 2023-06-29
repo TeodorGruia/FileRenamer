fd01d69ad6fd899b81d45023abc6fbd2575bd106
TeodorGruia <sjtg105@gmail.com>
2023-06-29 19:05:18 -0400

    Fixed issue with MP#s not showing up!

--- functions.py
+++ functions.py
@@ -2,15 +2,13 @@
 import os
 
 def find_music():
-    music_files = []
+    mus_ext = ['.mp3', '.wav', '.aac', '.m4a']
     working_dir = r'C:\\Users\\sjtg1\\Downloads'
     for path in os.listdir(working_dir):
         ext_only = os.path.splitext(path)[1]
-        if ext_only == ".mp3":
-            print(path)
-        else:
-            print("No Music")
-            break        
+        for mext in mus_ext:
+            if ext_only == mext:
+                print(os.path.join(path, ext_only))       
 def search_filename(filename):
     pass
 
